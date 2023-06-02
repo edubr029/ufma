6d81efe70cec9c3bd86e1ec1b2887a62f993f33b
Eduardo Fonseca <edubr029@gmail.com>
2023-06-02 05:40:50 -0300

    just ignore again

--- LPEE/exercise_list/functions.c
+++ LPEE/exercise_list/functions.c
@@ -2,11 +2,8 @@
 #include <stdlib.h>
 #include <locale.h>
 
-int invertNumber() {
-    int number, invertedNumber = 0, rest;
-
-    printf("Digite um número: ");
-    scanf("%d", &number);
+int invertNumber(int number) {
+    int invertedNumber = 0, rest;  
 
     while(number != 0) {
         rest = number % 10;
@@ -14,15 +11,50 @@ int invertNumber() {
         number /= 10;
     }
 
-    return invertedNumber;    
+    return invertedNumber;
+}
+
+double power(double base, double exponent) {
+    double result = 1;
+
+    if (exponent < 0) {
+        exponent *= -1;
+        base = 1 / base;
+    }
+
+    for (int i = 0; i < exponent; i++) {
+        result *= base;
+    }
+
+    return result;
 }
 
 int main() {
+    int invertedNumber, number;
+    double base, exponent, result;
+
     setlocale(LC_ALL, "Portuguese");
 
-    int invertedNumber = invertNumber();
+    //Número inteiro invertido
+
+    printf("Digite um número: ");
+    scanf("%d", &number);
+
+    invertedNumber = invertNumber(number);
 
     printf("O número invertido é: %d\n", invertedNumber);
+
+    //Função potência  
+
+    printf("Digite a base: ");
+    scanf("%lf", &base);
+
+    printf("Digite o expoente: ");
+    scanf("%lf", &exponent);
+
+    result = power(base, exponent);
+
+    printf("O resultado é: %.2lf\n", result);
 
     return 0;
 }
