c43a34a468c73bf9c385deb39c14554b183de938
Eduardo Fonseca <edubr029@gmail.com>
2023-06-29 08:46:17 -0300

    Linked list study

---
+++ LPEE/linked-list.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+#define MAX_SIZE 1024
+
+typedef struct node
+{
+    int data;
+    struct node *next;
+} node_t;
+
+node_t* insert(node_t *head, int new_data) {
+    node_t *new_node = (node_t*) malloc(sizeof(node_t));
+    if(new_node == NULL) {
+        printf("Erro de alocação.\n");
+        exit(0);
+    }
+    new_node->data = new_data;
+    new_node->next = head;
+    
+    return new_node;
+}
+
+node_t* order_insert(node_t *head, int new_data) {
+    node_t *new_node = (node_t*) malloc(sizeof(node_t));
+    if(new_node == NULL) {
+        printf("Erro de alocação.\n");
+        exit(0);
+    }
+    new_node->data = new_data;
+    new_node->next = NULL;
+    
+    if(head == NULL)
+        head = new_node;
+    else {
+        node_t *p = head;
+        while(p->next != NULL) {
+            p = p->next;
+        }
+        p->next = new_node;
+    }
+    
+    return head;
+}
+
+void createLinkedList(node_t **head) {
+    *head = order_insert(*head, 23);
+    *head = order_insert(*head, 45);
+    *head = order_insert(*head, 56);
+    *head = order_insert(*head, 78);
+}
+
+
+void printl(node_t *head) {
+    node_t *p;
+    for(p = head; p != NULL; p = p->next)
+        printf("info = %d, end = %x \n", p->data, p);
+}
+
+void freemem(node_t *head) {
+    node_t *p = head, *t;
+    while (p != NULL) {
+        t = p->next;
+        free(p);
+        p = t;
+    }
+    
+}
+
+int main() {
+    node_t *head = NULL;
+
+    createLinkedList(&head);
+    printl(head);
+
+    freemem(head);
+    return 0;
+}
