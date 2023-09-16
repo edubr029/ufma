0d730dae84853e5b0b809abcfaa2cf5a8ad3ea43
Eduardo Fonseca <edubr029@gmail.com>
2023-09-16 16:08:37 -0300

    Improvements

--- LPEE/hangman.c
+++ LPEE/hangman.c
@@ -5,9 +5,10 @@
 #include <time.h>
 
 #define MAX_SIZE 4096
-#define MAX_RND 6
+#define MAX_RND 10
+#define MAX_ATTEMPTS 6
 
-typedef struct hangman {
+typedef struct hangman_data {
     char word[MAX_SIZE];
     char theme[MAX_SIZE];
 } hm_t;
@@ -19,6 +20,47 @@ void savedWords(hm_t *word_list) {
     for(int i = 0; i < MAX_RND; i++) {
         strcpy(word_list[i].theme, themes[i]);
         strcpy(word_list[i].word, words[i]);
+    }
+}
+
+void defineGamemode(int *gamemode) {
+    int maxInputTry = 0;
+    do {
+        printf("Digite o modo de jogo\n");
+        printf("1 - Palavras salvas (1 jogador) | 2 - Palavras digitadas (2 jogadores): ");
+        scanf(" %d", gamemode);
+
+        if(*gamemode != 1 && *gamemode != 2) {
+            printf("Digite um número válido.\n");
+            maxInputTry++;
+        }
+        if(maxInputTry > 3) {
+            printf("Você excedeu o número máximo de tentativas.\n");
+            exit(1);
+        }
+    } while(*gamemode != 1 && *gamemode != 2);
+}
+
+void defineMaxRound(int *maxRound) {
+    do {
+        printf("Digite o número máximo de rodadas (1 a %d): ", MAX_RND);
+        scanf("%d", maxRound);
+
+        if(*maxRound < 1 || *maxRound > MAX_RND)
+            printf("Digite um número válido.\n");
+    } while(*maxRound < 1 || *maxRound > MAX_RND);
+}
+
+void readPlayerWords(hm_t *word_list) {
+    int maxRound;    
+    defineMaxRound(&maxRound);
+
+    for(int i = 0; i < maxRound; i++) {
+        printf("Digite a palavra da rodada %d: ", i + 1);
+        scanf("%s", word_list[i].word);
+
+        printf("Digite a tema da rodada %d: ", i + 1);
+        scanf("%s", word_list[i].theme);
     }
 }
 
@@ -37,8 +79,15 @@ void hideLetters(hm_t *word_list, int num, char *hidden) {
         printf("%c ", hidden[i]);
 }
 
-void hangman(int attempts) {
-    switch (attempts) {
+int checkLetter(char letter, char *guessedLetters, int attempts) {
+    for(int i = 0; i < attempts; i++)
+        if(guessedLetters[i] == letter)
+            return 1;
+    return 0;
+}
+
+void printHangman(int attempts) {
+    switch(attempts) {
         case 0:
             printf("\n\n");
             printf("  +---+\n");
@@ -112,18 +161,19 @@ void hangman(int attempts) {
     }
 }
 
-int checkLetter(char letter, char *guessedLetters, int attempts) {
-    for(int i = 0; i < attempts; i++)
-        if(guessedLetters[i] == letter)
-            return 1;
-    return 0;
-}
-
 void game(hm_t *word_list) {
     char answer = 'S';
+    int gamemode = 0;
+        
     while(answer == 'S' || answer == 's') {
-        int attempts = 0, num = rand_num();
-        savedWords(word_list);
+        int attempts = 0, num = rand_num(); 
+
+        if(gamemode == 0)
+            defineGamemode(&gamemode);
+        if(gamemode == 1)
+            savedWords(word_list);
+        if(gamemode == 2)
+            readPlayerWords(word_list);
         
         showTheme(word_list, num);
 
@@ -135,11 +185,13 @@ void game(hm_t *word_list) {
         hideLetters(word_list, num, hidden);
 
         int correct = 0;
-        char guessedLetters[6];
-        while(attempts < 6) {
+        char guessedLetters[MAX_ATTEMPTS];
+        while(attempts < MAX_ATTEMPTS) {
             char letter;
             printf("\n\nDigite uma letra: ");
             scanf(" %c", &letter);
+            int c;
+            while((c = getchar()) != '\n' && c != EOF);
 
             if((letter >= 'a' && letter <= 'z') || (letter >= 'A' && letter <= 'Z')) {
                 if(checkLetter(letter, guessedLetters, attempts)) {
@@ -169,14 +221,14 @@ void game(hm_t *word_list) {
                     }
                 } else {
                     attempts++;
-                    hangman(attempts);
+                    printHangman(attempts);
                 }
             } else {
                 printf("\nDigite uma letra válida.\n");
             }
         }
 
-        if(attempts == 6) {
+        if(attempts == MAX_ATTEMPTS) {
             printf("\n\nVocê perdeu!\n");
             printf("A palavra era: %s\n", word_list[num].word);
         }
